5d307210dec4890b5bc7d2fc746cb0f816aee662
Rikard-Sut <mailarikardskoglundnu@gmail.com>
2020-02-19 11:17:26 +0100

    Added codeshell for spot

--- ParkingLotLogic/ParkingSpot.cs
+++ ParkingLotLogic/ParkingSpot.cs
@@ -1,10 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using VehicleObjects;
 
 namespace ParkingLotLogic
 {
     class ParkingSpot
     {
+        int maxCapacity { get; set; }
+        int currentCapacity { get; set; }
+
+        List<IVehicle> vehiclesInSpot = new List<IVehicle>();
+
+        internal ParkingSpot()
+        {
+
+        }
+        internal bool AddVehicle(IVehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+        internal IVehicle RemoveVehicle(string regNum)
+        {
+            throw new NotImplementedException();
+        }
+        internal bool FindVehicle(string regNum)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object CloneSpot()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
