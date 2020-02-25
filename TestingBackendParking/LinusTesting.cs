82572968c570d3a5bf196140483d625f34a991e8
Linus Örnborg <linus.skola@ornborg.se>
2020-02-25 09:48:07 +0100

    remove .bat

--- TestingBackendParking/LinusTesting.cs
+++ TestingBackendParking/LinusTesting.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestingBackendParking
 {
     public partial class UnitTesting
     {
-        private void FuckOff()
+        [TestMethod]
+        public void FuckOff()
         {
             Console.WriteLine("Dont run my fucking method!");
+            int x = 1;
+            int y = 1;
+
+            //Assert.AreSame == minnesplats
+            //Assert.AreE
+
+            //Assert.AreSame(x, y);
+            Assert.AreEqual(x, y);
         }
     }
 }
