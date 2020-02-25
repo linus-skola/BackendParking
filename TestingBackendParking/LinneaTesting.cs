9b69628e89147e8c0093a82f39582af99d1f456e
Linnea Smedberg <linnea.smedberg@live.se>
2020-02-25 09:48:53 +0100

    Added test methods

--- TestingBackendParking/LinneaTesting.cs
+++ TestingBackendParking/LinneaTesting.cs
@@ -1,14 +1,57 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkingLotLogic;
+using VehicleObjects;
 
 namespace TestingBackendParking
 {
     [TestClass]
     public class LinneaTesting
     {
+        // Fix
+        [DataRow(0, 0)]
+        [DataRow(5, 60)]
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddVehicleParkingLot()
         {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void TestFindSpotParkingLot()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void TestRemoveVehicleParkingLot()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void TestSearchVehicleParkingLot()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void TestParkingSpotContentParkingLot()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void TestOptimizeParkingLotParkingLot()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestMethod]
+        public void TestAddParkingSpotParkingLot()
+        {
+            Assert.Inconclusive();
         }
     }
 }
